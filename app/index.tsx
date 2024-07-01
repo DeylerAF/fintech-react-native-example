b47e61d31f669451a6cbb61cb581143901566a07
DeylerAF <deylerafernandez@outlook.com>
2024-07-01 14:47:53 -0600

    Update dependencies and remove unused code

---
+++ app/index.tsx
@@ -0,0 +1,58 @@
+import { View, Text, StyleSheet, Touchable } from "react-native";
+import React from "react";
+import { useAssets } from "expo-asset";
+import { Video } from "expo-av";
+import { TouchableOpacity } from "react-native";
+
+const Page = () => {
+  const [assets] = useAssets([require("@/assets/videos/intro.mp4")]);
+
+  return (
+    <View style={styles.container}>
+      {assets && (
+        <Video
+          isMuted
+          isLooping
+          shouldPlay
+          source={{ uri: assets[0].uri }}
+          style={styles.video}
+        />
+      )}
+      <View style={{ marginTop: 80, padding: 20 }}>
+        <Text style={styles.header}>Ready to change the way you money?</Text>
+      </View>
+
+      <View style={styles.buttons}>
+        <TouchableOpacity>
+          <Text style={{ color: "white" }}>Get Started</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  video: {
+    width: "100%",
+    height: "100%",
+    position: "absolute",
+  },
+  header: {
+    fontSize: 36,
+    fontWeight: "900",
+    textTransform: "uppercase",
+    color: "white",
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    gap: 20,
+  },
+});
+
+export default Page;
